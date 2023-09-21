f500ef363aa5343259815c9a3d95e7457b5b6606
Samuel <omilosamuel@gmail.com>
2023-09-21 18:11:38 +0100

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import logger from 'loglevel'
 import {startServer} from './start'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const isTest = process.env.NODE_ENV === 'test'
 const logLevel = process.env.LOG_LEVEL || (isTest ? 'warn' : 'info')
